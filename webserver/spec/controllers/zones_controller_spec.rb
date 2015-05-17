a9bac8109b06b02698e8d75b83e93e7dd4f624df
Horacio Branciforte <horacio.branciforte@mercadolibre.com>
2015-05-17 13:31:09 -0300

    fix controller test

--- webserver/spec/controllers/zones_controller_spec.rb
+++ webserver/spec/controllers/zones_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ZonesController, type: :controller do
   describe '#new zone' do
     it 'creates new zone' do
       post 'create', :zone => {name: 'testing', zone_number: 400 }
-      expect(response).to have_http_status(:created)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
