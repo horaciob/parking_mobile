5f6cb7c75e33c3652f800d8554731c92cf409a01
Horacio Branciforte <horacio.branciforte@mercadolibre.com>
2015-05-24 09:07:29 -0300

    add zone test

--- webserver/spec/controllers/zones_controller_spec.rb
+++ webserver/spec/controllers/zones_controller_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe ZonesController, type: :controller do
-  describe '#new zone' do
-    it 'creates new zone' do
-      post 'create', :zone => {name: 'testing', zone_number: 400 }
-      expect(response).to have_http_status(:ok)
+  describe '#create' do
+    context "creating zones" do
+      it 'creates new zone' do
+        post 'create', :zone => {name: 'testing', zone_number: 400 }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    describe '#modify' do
+      subject { creates(:zone) }
+      it 'update attributes' do
+
+      end
     end
   end
 end
