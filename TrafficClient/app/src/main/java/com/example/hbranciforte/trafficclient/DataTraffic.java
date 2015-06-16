250cf012b9c7a7fc0ba0dfa4d9746e1f2eda01c9
horaciob <horaciob@gmail.com>
2015-06-15 21:19:52 -0300

    add listener

--- TrafficClient/app/src/main/java/com/example/hbranciforte/trafficclient/DataTraffic.java
+++ TrafficClient/app/src/main/java/com/example/hbranciforte/trafficclient/DataTraffic.java
@@ -4,9 +4,27 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DataTraffic extends ActionBarActivity {
@@ -16,6 +34,23 @@ public class DataTraffic extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_traffic);
+        EditText ed_fichas = (EditText) findViewById(R.id.fichas);
+        ed_fichas.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                           }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView txtvalor = (TextView) findViewById(R.id.valor);
+                txtvalor.setText("aaa")
+            }
+        });
     }
 
     @Override
@@ -41,10 +76,63 @@ public class DataTraffic extends ActionBarActivity {
     }
 
     public void lunchResume(View view) {
+        new MyAsyncTask().execute("aa");
         Intent intent = new Intent(this, Resume.class);
         startActivity(intent);
     }
+    public void postData(String valueIWantToSend) {
+        HttpClient httpclient = new DefaultHttpClient();
+        // specify the URL you want to post to
+        HttpPost httppost = new HttpPost("http://10.0.2.2:3000/parking");
+        try {
+            // create a list to store HTTP variables and their values
+            List nameValuePairs = new ArrayList();
+            // add an HTTP variable and value pair
+            nameValuePairs.add(new BasicNameValuePair("myHttpData", "value"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+            // send the variable and value, in other words post, to the URL
+            HttpResponse response = httpclient.execute(httppost);
+        } catch (ClientProtocolException e) {
+            e.getStackTrace();
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+    private class MyAsyncTask extends AsyncTask<String, Integer, Double>{
 
+        @Override
+        protected Double doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            postData(params[0]);
+            return null;
+        }
 
+        protected void onPostExecute(Double result){
+            Toast.makeText(getApplicationContext(), "command sent", Toast.LENGTH_LONG).show();
+        }
+        protected void onProgressUpdate(Integer... progress){
+        }
 
+        public void postData(String valueIWantToSend) {
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://somewebsite.com/receiver.php");
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("myHttpData", valueIWantToSend));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+        }
+
+    }
 }
