9e5da41d90c2926cb9c05f782d2d88f6490bdc35
horaciob <horaciob@gmail.com>
2015-06-28 12:37:20 -0300

    puma

--- webserver/config/puma.rb
+++ webserver/config/puma.rb
@@ -1,3 +1,5 @@
+# Production config, if you want to use on development
+# just run puma into working directory
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
@@ -6,7 +8,7 @@ preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || 'production'
 
 on_worker_boot do
   ActiveRecord::Base.establish_connection
