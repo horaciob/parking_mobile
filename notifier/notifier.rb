8bfd10312c6433d6443562b963a472bc209ea21d
horaciob <horaciob@gmail.com>
2015-06-29 01:27:24 -0300

    add log on notifier

--- notifier/notifier.rb
+++ notifier/notifier.rb
@@ -1,14 +1,17 @@
 #!/usr/bin/env ruby
-require "typhoeus"
-require "time"
+require 'typhoeus'
+require 'time'
 require 'json'
+require 'logger'
 
 URL =  "http://localhost:3000"
 timestamp=(Time.now+3*60*60).strftime("%Y%m%d%H%M")
 parkings=JSON.parse(Typhoeus.get("#{URL}/parkings?expires=#{timestamp}&status=allowed").body)
 
+log = Logger.new('/tmp/logger.log')
+
 parkings.each do |p|
-  puts p["id"].to_s
+  log.info "#{p['id']} #{p['expires_at']}
   Typhoeus.put("http://localhost:3000/parkings/#{p["id"]}",
                headers: {'Content-Type'=> "application/json"},
                body: '{"status":"expired"}'
