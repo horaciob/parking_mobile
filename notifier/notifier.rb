0b1843df4e819b86d681894ce811e4a0bb6a7dc6
horaciob <horaciob@gmail.com>
2015-06-29 01:35:08 -0300

    fix notifier

--- notifier/notifier.rb
+++ notifier/notifier.rb
@@ -4,16 +4,16 @@ require 'time'
 require 'json'
 require 'logger'
 
-URL =  "http://localhost:3000"
+URL = 'http://localhost:3000'
 timestamp=(Time.now+3*60*60).strftime("%Y%m%d%H%M")
 parkings=JSON.parse(Typhoeus.get("#{URL}/parkings?expires=#{timestamp}&status=allowed").body)
-
-log = Logger.new('/tmp/logger.log')
+log = Logger.new('/tmp/logger.log',7,50000)
 
 parkings.each do |p|
-  log.info "#{p['id']} #{p['expires_at']}
+  log.info "parking id: #{p['id']}   expires_at: #{p['expires_at']}"
   Typhoeus.put("http://localhost:3000/parkings/#{p["id"]}",
                headers: {'Content-Type'=> "application/json"},
                body: '{"status":"expired"}'
               )
 end
+
